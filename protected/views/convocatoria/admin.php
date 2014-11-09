41794e5b7d2b689145f2d5e8ef16933d7b1d2a47
Johansebastian1 <johansebastiangh@gmail.com>
2014-11-08 21:40:53 -0500

    Modificacion Vista Manage Convocatoria Version 3
    Modificacion Vista Manage Convocatoria Version 3

--- protected/views/convocatoria/admin.php
+++ protected/views/convocatoria/admin.php
@@ -44,7 +44,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	'id'=>'convocatoria-grid',
 	'dataProvider'=>$model->search(),
 	'filter'=>$model,
-	'columns'=>array
+	'columns'=>array(
 		'convocatoria',
 		'tipo_convocatoria',
 		'subtipo_convocatoria',
