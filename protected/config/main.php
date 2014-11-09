56e04231be76d6e5ac17da301927fee2730b18b3
Johansebastian1 <johansebastiangh@gmail.com>
2014-11-08 22:46:56 -0500

    Cambio color

--- protected/config/main.php
+++ protected/config/main.php
@@ -58,7 +58,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=mydb',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => '12345',
 			'charset' => 'utf8',
 		),
 
