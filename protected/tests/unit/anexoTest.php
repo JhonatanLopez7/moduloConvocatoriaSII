559c553c4447a4d11239fbb2ef5b928fe290999c
JhonatanLopez7 <jsteven2372@gmail.com>
2014-11-20 02:01:58 -0500

    Prueba Anexo

--- protected/tests/unit/anexoTest.php
+++ protected/tests/unit/anexoTest.php
@@ -2,6 +2,43 @@
 class anexoTest extends PHPUnit_Framework_TestCase{
 	
 	public function testCreate(){
+
+		$convocatoria = new Convocatoria;
+		$convocatoria->tipo_convocatoria='Proyectos';
+ 		$convocatoria->subtipo_convocatoria='Investigacion';
+ 		$convocatoria->convocatoria_programa='Si';
+		$convocatoria->tipo_financiacion='Cofinanciacion';
+ 		$convocatoria->fecha_apertura='2014-11-19';
+ 		$convocatoria->fecha_cierre='2014-11-19';
+		$convocatoria->fecha_resultado_preliminar='2014-11-19';
+		$convocatoria->fecha_resultado_definitivo='2014-11-19';
+		$convocatoria->convocatoria='convocatoria 1';
+		$convocatoria->codigo_convocatoria=1;
+		$convocatoria->descripcion='Convocatoria para jovenes Investigadores';
+		$convocatoria->objeto='Universidades del pais';
+		$convocatoria->programa_base='Ingenieria';
+		$convocatoria->instituciones_financiadoras='Gobernacion';
+		$convocatoria->maximo_porcentaje_financiar=100;
+	 	$convocatoria->minimo_porcentaje_financiar=70;
+ 		$convocatoria->minimo_porcentaje_contrapartida_dinero=100;
+		$convocatoria->minimo_porcentaje_contrapartida_especie=100;
+		$convocatoria->monto_maximo_financiar=8000000000;
+ 		$convocatoria->numero_maximo_meses_proyecto=2;
+ 		$convocatoria->monto_total_convocatoria=30000000000;
+		$convocatoria->convenio='Institucional';
+		$convocatoria->cdp=1;
+ 		$convocatoria->fecha_cdp='2014-11-19';
+		$convocatoria->cdr=1;
+		$convocatoria->fecha_cdr='2014-11-19';
+		$convocatoria->url='www.colciencias.com.co';
+		$convocatoria->linea_investigacion='Gran linea';
+		$convocatoria->postulacion_anticipada='Si';
+		$convocatoria->definir_estructura_convocatoria='Plantilla1';
+		$convocatoria->numero_proyecto_inversion=1;
+		$convocatoria->vigencia=1;
+
+		$this->assertTrue($convocatoria->save());
+
 		$anexo=new Anexo();
 		$anexo->codigo=2;
 		$anexo->convocatoria=1;
@@ -11,8 +48,46 @@ class anexoTest extends PHPUnit_Framework_TestCase{
 
 		$this->assertTrue($anexo->save());
 		$anexo->delete();
+		$convocatoria->delete();
 	}
 	public function testBuscar(){
+
+		$convocatoria = new Convocatoria;
+		$convocatoria->tipo_convocatoria='Proyectos';
+ 		$convocatoria->subtipo_convocatoria='Investigacion';
+ 		$convocatoria->convocatoria_programa='Si';
+		$convocatoria->tipo_financiacion='Cofinanciacion';
+ 		$convocatoria->fecha_apertura='2014-11-19';
+ 		$convocatoria->fecha_cierre='2014-11-19';
+		$convocatoria->fecha_resultado_preliminar='2014-11-19';
+		$convocatoria->fecha_resultado_definitivo='2014-11-19';
+		$convocatoria->convocatoria='convocatoria 1';
+		$convocatoria->codigo_convocatoria=1;
+		$convocatoria->descripcion='Convocatoria para jovenes Investigadores';
+		$convocatoria->objeto='Universidades del pais';
+		$convocatoria->programa_base='Ingenieria';
+		$convocatoria->instituciones_financiadoras='Gobernacion';
+		$convocatoria->maximo_porcentaje_financiar=100;
+	 	$convocatoria->minimo_porcentaje_financiar=70;
+ 		$convocatoria->minimo_porcentaje_contrapartida_dinero=100;
+		$convocatoria->minimo_porcentaje_contrapartida_especie=100;
+		$convocatoria->monto_maximo_financiar=8000000000;
+ 		$convocatoria->numero_maximo_meses_proyecto=2;
+ 		$convocatoria->monto_total_convocatoria=30000000000;
+		$convocatoria->convenio='Institucional';
+		$convocatoria->cdp=1;
+ 		$convocatoria->fecha_cdp='2014-11-19';
+		$convocatoria->cdr=1;
+		$convocatoria->fecha_cdr='2014-11-19';
+		$convocatoria->url='www.colciencias.com.co';
+		$convocatoria->linea_investigacion='Gran linea';
+		$convocatoria->postulacion_anticipada='Si';
+		$convocatoria->definir_estructura_convocatoria='Plantilla1';
+		$convocatoria->numero_proyecto_inversion=1;
+		$convocatoria->vigencia=1;
+
+		$this->assertTrue($convocatoria->save());
+
 		$anexo=new Anexo();
 		$anexo->codigo=2;
 		$anexo->convocatoria=1;
@@ -24,8 +99,46 @@ class anexoTest extends PHPUnit_Framework_TestCase{
 	 	$aux = $anexo->findByPk(2);
 	 	$this->assertNotNull($aux);
 	 	$anexo->delete();
+	 	$convocatoria->delete();
 	}
 	public function testUpdate(){
+
+		$convocatoria = new Convocatoria;
+		$convocatoria->tipo_convocatoria='Proyectos';
+ 		$convocatoria->subtipo_convocatoria='Investigacion';
+ 		$convocatoria->convocatoria_programa='Si';
+		$convocatoria->tipo_financiacion='Cofinanciacion';
+ 		$convocatoria->fecha_apertura='2014-11-19';
+ 		$convocatoria->fecha_cierre='2014-11-19';
+		$convocatoria->fecha_resultado_preliminar='2014-11-19';
+		$convocatoria->fecha_resultado_definitivo='2014-11-19';
+		$convocatoria->convocatoria='convocatoria 1';
+		$convocatoria->codigo_convocatoria=1;
+		$convocatoria->descripcion='Convocatoria para jovenes Investigadores';
+		$convocatoria->objeto='Universidades del pais';
+		$convocatoria->programa_base='Ingenieria';
+		$convocatoria->instituciones_financiadoras='Gobernacion';
+		$convocatoria->maximo_porcentaje_financiar=100;
+	 	$convocatoria->minimo_porcentaje_financiar=70;
+ 		$convocatoria->minimo_porcentaje_contrapartida_dinero=100;
+		$convocatoria->minimo_porcentaje_contrapartida_especie=100;
+		$convocatoria->monto_maximo_financiar=8000000000;
+ 		$convocatoria->numero_maximo_meses_proyecto=2;
+ 		$convocatoria->monto_total_convocatoria=30000000000;
+		$convocatoria->convenio='Institucional';
+		$convocatoria->cdp=1;
+ 		$convocatoria->fecha_cdp='2014-11-19';
+		$convocatoria->cdr=1;
+		$convocatoria->fecha_cdr='2014-11-19';
+		$convocatoria->url='www.colciencias.com.co';
+		$convocatoria->linea_investigacion='Gran linea';
+		$convocatoria->postulacion_anticipada='Si';
+		$convocatoria->definir_estructura_convocatoria='Plantilla1';
+		$convocatoria->numero_proyecto_inversion=1;
+		$convocatoria->vigencia=1;
+
+		$this->assertTrue($convocatoria->save());
+
 		$anexo=new Anexo();
 		$anexo->codigo=2;
 		$anexo->convocatoria=1;
@@ -38,8 +151,46 @@ class anexoTest extends PHPUnit_Framework_TestCase{
 	 	$aux->nombre='convocatoria nueva';
 	 	$this->assertTrue($anexo->update());
 	 	$aux->delete();
+	 	$convocatoria->delete();
 	}
 	public function testDelete(){
+
+		$convocatoria = new Convocatoria;
+		$convocatoria->tipo_convocatoria='Proyectos';
+ 		$convocatoria->subtipo_convocatoria='Investigacion';
+ 		$convocatoria->convocatoria_programa='Si';
+		$convocatoria->tipo_financiacion='Cofinanciacion';
+ 		$convocatoria->fecha_apertura='2014-11-19';
+ 		$convocatoria->fecha_cierre='2014-11-19';
+		$convocatoria->fecha_resultado_preliminar='2014-11-19';
+		$convocatoria->fecha_resultado_definitivo='2014-11-19';
+		$convocatoria->convocatoria='convocatoria 1';
+		$convocatoria->codigo_convocatoria=1;
+		$convocatoria->descripcion='Convocatoria para jovenes Investigadores';
+		$convocatoria->objeto='Universidades del pais';
+		$convocatoria->programa_base='Ingenieria';
+		$convocatoria->instituciones_financiadoras='Gobernacion';
+		$convocatoria->maximo_porcentaje_financiar=100;
+	 	$convocatoria->minimo_porcentaje_financiar=70;
+ 		$convocatoria->minimo_porcentaje_contrapartida_dinero=100;
+		$convocatoria->minimo_porcentaje_contrapartida_especie=100;
+		$convocatoria->monto_maximo_financiar=8000000000;
+ 		$convocatoria->numero_maximo_meses_proyecto=2;
+ 		$convocatoria->monto_total_convocatoria=30000000000;
+		$convocatoria->convenio='Institucional';
+		$convocatoria->cdp=1;
+ 		$convocatoria->fecha_cdp='2014-11-19';
+		$convocatoria->cdr=1;
+		$convocatoria->fecha_cdr='2014-11-19';
+		$convocatoria->url='www.colciencias.com.co';
+		$convocatoria->linea_investigacion='Gran linea';
+		$convocatoria->postulacion_anticipada='Si';
+		$convocatoria->definir_estructura_convocatoria='Plantilla1';
+		$convocatoria->numero_proyecto_inversion=1;
+		$convocatoria->vigencia=1;
+
+		$this->assertTrue($convocatoria->save());
+
 		$anexo=new Anexo();
 		$anexo->codigo=2;
 		$anexo->convocatoria=1;
@@ -49,6 +200,7 @@ class anexoTest extends PHPUnit_Framework_TestCase{
 		
 		$this->assertTrue($anexo->save());
 	 	$this->assertTrue($anexo->delete());
+	 	$convocatoria->delete();
 	}
 }	
 ?>
